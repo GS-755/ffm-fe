33bce3cd2dbe930ef906dd6f5854ea845a252152
Nguyễn Minh Trí <21dh114236@st.huflit.edu.vn>
2023-11-05 16:20:04 +0700

    Update Thuyen assets & components

--- src/layout/Footer.tsx
+++ src/layout/Footer.tsx
@@ -1,8 +1,40 @@
 const Footer = () => {
   return (
-    <footer>
-      <p>:D</p> 
-    </footer>
+    <div id="div-footer">
+      <div className="footer-left"></div>
+      <div className="footer-center">
+        <h4>Hỗ trợ khách hàng</h4>
+        <p>Câu hỏi thường gặp</p>
+        <p>Điều khoản dịch vụ</p>
+      </div>
+      <div className="footer-right">
+        <h4>CHÍNH SÁCH</h4>
+        <p>Chính sách bảo mật</p>
+        <p>Chính sách thanh toán</p>
+      </div>
+      <div className="footer-left1">
+        <h4>Liên hệ</h4>
+        <p>Địa chỉ: FFM</p>
+        <p>Email: 
+          <a href="mailto:21dh114236@st.huflit.edu.vn">21dh114236@st.huflit.edu.vn</a>
+        </p>
+        <p>Số điện thoại: 
+          <a href="tel:0832476086">083 247 6086</a>
+        </p>
+      </div>
+      <div className="center">
+        <div className="footer-center1">
+          <h4>Đăng kí nhận tin</h4>
+          <p>Hãy nhập email của bạn<br />vào ô dưới đây để có thể<br /> nhận được tin tức mới nhất</p>
+        </div>
+      </div>
+      <div className="footer-right1">
+        <h4>Kết nối</h4>
+        <p>
+          <a href="">Hồ Hải Thuyên</a>
+        </p>
+      </div>
+    </div>
   )
 }
 
--- src/layout/Navbar.tsx
+++ src/layout/Navbar.tsx
@@ -1,14 +1,75 @@
+import "../style/thuyen/AcountStyle.css";
+import "../style/thuyen/DetailsStyle.css";
+import "../style/thuyen/Font.css";
+import "../style/thuyen/FooterStyle.css";
+import "../style/thuyen/inline.css";
+import "../style/thuyen/LayoutCart.css";
+import "../style/thuyen/LayoutStyle.css";
+import "../style/thuyen/menu.css";
+import "../style/thuyen/mystyle.css";
+import "../style/thuyen/PagedList.css";
+import "../style/thuyen/PageStyle.css";
+import "../style/thuyen/ProductsStyle.css";
+import "../style/thuyen/profile.css";
+import "../style/thuyen/ProViewStyle.css";
+import "../style/thuyen/Site.css";
+import "../style/thuyen/StyleWeb.css";
+
 import { Link } from "react-router-dom";
 
 const Navbar = () => {
-  return (
-    <>
-      <nav>
-        <Link to="/">Trang chủ fast</Link>
-        <Link to="/about">Giới thiệu fast</Link>
-      </nav>
-    </>
-  );
+    return (
+        <>
+            <div id="div-top">
+                <div className="wrapper">
+                    <div className="TenWeb">
+                        <a href="#" className="link">
+                            <h4 className="brand-name">BookStore PTLN</h4>
+                        </a>
+                    </div>
+                    <div className="text brand-name">
+                        <a href="#">Đăng nhập</a> | 
+                        <a href="#"> Đăng ký</a>
+                    </div>
+                </div>
+            </div>
+            <div id="nav">
+                <div className="wrapper">
+                    <div className="wrap">
+                        <form>
+                            <div className="search">
+                                <input id="searching" className="searchTerm" type="text" name="searching" placeholder="Tìm Kiếm truyện" />
+                                <button type="submit" className="searchButton">
+                                    <i className="fa-solid fa-magnifying-glass"></i>
+                                </button>
+                            </div>
+                        </form>
+                    </div>
+                    <div className="text brand-name-dark">
+                        <a href="#">Giỏ hàng</a>
+                        <a href="#" className="cl-browncyan"><i className="fa-solid fa-cart-shopping"></i></a>
+                        <span className="badge cart-number">(999)</span>
+                    </div>
+                </div>
+            </div>
+            <div id="horizontalmenu">
+                <ul className="ml-35rem">
+                    <li className="menu-item">
+                        <Link to="/">TRANG CHỦ</Link>
+                    </li>
+                    <li className="menu-item">
+                        <a href="#">DANH MỤC</a>
+                    </li>
+                    <li className="menu-item">
+                        <Link to="/about">GIỚI THIỆU</Link>
+                    </li>
+                    <li className="menu-item">
+                        <a href="#div-footer">LIÊN HỆ</a>
+                    </li>
+                </ul>
+            </div>
+        </>
+    );
 }
 
 export default Navbar;
