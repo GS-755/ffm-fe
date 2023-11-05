aa287dac4414ce6b97f69d6a10b1605e6f6696e8
g2asell2019 <51025883+g2asell2019@users.noreply.github.com>
2023-11-05 17:36:01 +0700

    remove unnecessary css import

--- src/layout/Navbar.tsx
+++ src/layout/Navbar.tsx
@@ -1,19 +1,19 @@
-import "../style/thuyen/AcountStyle.css";
-import "../style/thuyen/DetailsStyle.css";
-import "../style/thuyen/Font.css";
-import "../style/thuyen/FooterStyle.css";
-import "../style/thuyen/inline.css";
-import "../style/thuyen/LayoutCart.css";
-import "../style/thuyen/LayoutStyle.css";
-import "../style/thuyen/menu.css";
-import "../style/thuyen/mystyle.css";
-import "../style/thuyen/PagedList.css";
-import "../style/thuyen/PageStyle.css";
-import "../style/thuyen/ProductsStyle.css";
-import "../style/thuyen/profile.css";
-import "../style/thuyen/ProViewStyle.css";
-import "../style/thuyen/Site.css";
-import "../style/thuyen/StyleWeb.css";
+// import "../style/thuyen/AcountStyle.css";
+// import "../style/thuyen/DetailsStyle.css";
+// import "../style/thuyen/Font.css";
+// import "../style/thuyen/FooterStyle.css";
+// import "../style/thuyen/inline.css";
+// import "../style/thuyen/LayoutCart.css";
+// import "../style/thuyen/LayoutStyle.css";
+// import "../style/thuyen/menu.css";
+// import "../style/thuyen/mystyle.css";
+// import "../style/thuyen/PagedList.css";
+// import "../style/thuyen/PageStyle.css";
+// import "../style/thuyen/ProductsStyle.css";
+// import "../style/thuyen/profile.css";
+// import "../style/thuyen/ProViewStyle.css";
+// import "../style/thuyen/Site.css";
+// import "../style/thuyen/StyleWeb.css";
 
 import { Link } from "react-router-dom";
 
