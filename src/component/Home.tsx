d2753bef7796790f5ee9c73871e76dc22d97e901
Nguyễn Minh Trí <21dh114236@st.huflit.edu.vn>
2023-11-05 16:21:02 +0700

    Add margin for home component

--- src/component/Home.tsx
+++ src/component/Home.tsx
@@ -2,7 +2,7 @@ import { MainLayout } from "../layout/Layout"
 function Home() {
   return (
     <MainLayout>
-      <div>
+      <div className="container m-auto">
         <h1>Hello, world!</h1>
         <p>Do something big boiz :)</p>
       </div>
