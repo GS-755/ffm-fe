30e8006bfad197da344e71ef37e078c9e256793c
Nguyễn Minh Trí <21dh114236@st.huflit.edu.vn>
2023-11-05 16:27:28 +0700

    Update import because skill issue

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -1,6 +1,22 @@
 import { Footer } from './Footer';
 import { ReactNode } from 'react';
 import Navbar from './Navbar';
+import "../style/thuyen/AcountStyle.css";
+import "../style/thuyen/DetailsStyle.css";
+import "../style/thuyen/Font.css";
+import "../style/thuyen/FooterStyle.css";
+import "../style/thuyen/inline.css";
+import "../style/thuyen/LayoutCart.css";
+import "../style/thuyen/LayoutStyle.css";
+import "../style/thuyen/menu.css";
+import "../style/thuyen/mystyle.css";
+import "../style/thuyen/PagedList.css";
+import "../style/thuyen/PageStyle.css";
+import "../style/thuyen/ProductsStyle.css";
+import "../style/thuyen/profile.css";
+import "../style/thuyen/ProViewStyle.css";
+import "../style/thuyen/Site.css";
+import "../style/thuyen/StyleWeb.css";
 
 const MainLayout = ({ children }: { children: ReactNode }) => {
   return (
