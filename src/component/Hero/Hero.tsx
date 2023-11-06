91c0e77da28634bb59cc081592168c3a60c7ba60
Nguyễn Minh Trí <21dh114236@st.huflit.edu.vn>
2023-11-07 06:33:35 +0700

    Update Hero picture

--- src/component/Hero/Hero.tsx
+++ src/component/Hero/Hero.tsx
@@ -1,6 +1,8 @@
 import { Carousel, initTE } from "tw-elements";
 initTE({ Carousel });
 import HeroImg from "../../assets/image/hero/unsplash-hero.jpg";
+import HeroImg2 from "../../assets/image/hero/unsplash-hero-2.jpg";
+
 
 const Hero = () => {
   return (
@@ -16,7 +18,7 @@ const Hero = () => {
               <img src={HeroImg} className="d-block w-100 h-450" alt="..." />
             </div>
             <div id="Banner" className="carousel-item" data-bs-interval="3000">
-              <img src={HeroImg} className="d-block w-100" alt="..." />
+              <img src={HeroImg2} className="d-block w-100" alt="..." />
             </div>
           </div>
           <button className="carousel-control-prev" type="button" data-bs-target="#Banner" data-bs-slide="prev">
