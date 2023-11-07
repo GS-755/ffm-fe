42e272fae5bd58ae8334d498b43fcc1f731549fc
Nguyễn Minh Trí <21dh114236@st.huflit.edu.vn>
2023-11-07 07:02:46 +0700

    Fix detail.tsx glitches

--- src/component/Detail.tsx
+++ src/component/Detail.tsx
@@ -1,9 +1,11 @@
 import { MainLayout } from "../layout/Layout";
+import { BtnBuy } from "../ui/ButtonBuy";
+import { BtnAddToCart } from "../ui/ButtonToCart";
 import FakeProduct from "../utils/fake-product";
+import { formatCurrency } from "../utils/format-currency";
 
 
 function Detail() {
-    
     const fakeHotProducts: FakeProduct = 
         {
           proId: 1,
@@ -11,21 +13,32 @@ function Detail() {
           proDescription: "Mô tả #1",
           proPrice: 27000
         }
-
   return (
     <MainLayout>
-        <div className ="container h-screen mx-32 bg-gray-100 w-auto">
+        <div className ="container h-screen mx-32 w-auto">
             <div className="grid grid-cols-2 gap-5">
                 <img className="mt-4 rounded-lg" src="https://images.foody.vn/res/g104/1034432/prof/s640x400/foody-upload-api-foody-mobile-40-200706093715.jpg"></img>
                 <div className="mt-4">
                     <div className=" mx-3 font-semibold">
-                        <h2 className="bg-[#D9D9D9] my-2 font-semibold text-2xl pl-5 py-1 rounded-lg">{fakeHotProducts.proTitle}</h2>
-                        <h2 className="bg-[#D9D9D9] my-2 font-semibold text-2xl pl-5 py-1 rounded-lg">{fakeHotProducts.proPrice}</h2>
-                        <h2 className="bg-[#D9D9D9] my-2 font-semibold text-2xl pl-5 py-1 rounded-lg">Mô tả</h2>
-                        <h2 className="bg-orange-200 my-2 font-semibold text-center rounded-lg h-28">{fakeHotProducts.proDescription}</h2>
+                        <h2 className="my-2 font-semibold text-3xl pl-5 py-1 rounded-lg">{fakeHotProducts.proTitle}</h2>
+                        <h2 className="my-2 font-semibold text-4xl pl-5 py-1 text-danger">{formatCurrency(fakeHotProducts.proPrice)}</h2>
+                        <h2 className="my-2 font-semibold text-2xl text-center text-uppercase">Mô tả món ăn</h2>
+                        <div className="bg-orange-100 p-4 rounded-lg">
+                            <div className="row">
+                                <p className="mb-1">{fakeHotProducts.proDescription}</p>
+                                <p>
+                                    Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                                    Illo alias, libero reiciendis voluptates cumque qui reprehenderit 
+                                    repellat rerum maiores aliquid at nesciunt modi blanditiis. 
+                                    Architecto facere error odio earum similique.
+                                </p>
+                            </div>
+                        </div>
+                        <div className="mt-4 grid grid-cols-2 gap-3">
+                            <BtnBuy></BtnBuy>
+                            <BtnAddToCart></BtnAddToCart>
+                        </div>
                     </div>
-                <button className="bg-yellow-300 p-2 rounded-md ml-28 font-semibold">Thêm vào giỏ hàng</button>
-                <button className="bg-yellow-300 p-2 rounded-md ml-28 font-semibold">Mua ngay</button>
                 </div>
             </div>
         </div>
--- src/component/Home.tsx
+++ src/component/Home.tsx
@@ -1,26 +1,10 @@
 import { MainLayout } from "../layout/Layout";
-function Product({ name, image, price }: { name: string, image: string, price: number }) {
-  return (
-    <div className="product">
-      <img src={image} alt={name} />
-      <h3>{name}</h3>
-      <p>{price}</p>
-    </div>
-  );
-}
 import "../style/App.css";
-import { MainLayout } from "../layout/Layout";
 import { Hero } from "./Hero";
 import { Product } from "./Product/Product";
 import { HotProduct } from "./Product/HotProduct";
 
 function Home() {
-  // Dữ liệu sản phẩm bán chạy (ví dụ)
-  const bestSellingProducts = [
-    { name: "Sản phẩm 1", image: "https://i.imgur.com/QPxY57r.jpg", price: 100 },
-    { name: "Sản phẩm 2", image: "https://i.imgur.com/jYijkUy.jpg", price: 150 },
-    // Thêm các sản phẩm khác ở đây
-  ];
   return (
     <MainLayout>
       <Hero></Hero>
